74c655d529d8e71ccca90773c2c6e112a240666b
bin.zeng <596659597@qq.com>
2025-03-03 00:28:14 +0800

    update

--- src/pages/Dashboard/index.jsx
+++ src/pages/Dashboard/index.jsx
@@ -1,7 +1,36 @@
-export default () => {
+import React from 'react'
+import {Carousel} from 'antd'
+import './style.css'
+
+const imgs = [
+  '/images/slide1.jpeg',
+  '/images/slide2.jpeg',
+  '/images/slide3.jpeg',
+  '/images/slide4.jpeg',
+]
+
+
+class Home extends React.Component {
+  render() {
     return (
-        <>
-            首页
-        </>
+      <div style={styles.bg} className='home'>
+        <Carousel arrows effect='fade' className='size'>
+          {imgs.map(item=><div key={item}><div className='size' style={{backgroundImage:`url(${item})`}}/></div>)}
+          {/*不用img标签是因为图片大小会出现问题*/}
+        </Carousel>
+      </div>
     )
+  }
 }
+
+const styles = {
+  bg:{
+    position:'absolute',
+    top:0,
+    left:0,
+    width:'100%',
+    height:'100%'
+  }
+}
+
+export default Home
