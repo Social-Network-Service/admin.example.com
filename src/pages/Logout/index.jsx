5a15ed01aa0396d59a7805978f883cd193cf16d0
bin.zeng <596659597@qq.com>
2025-03-11 23:03:00 +0800

    update

--- src/pages/Logout/index.jsx
+++ src/pages/Logout/index.jsx
@@ -10,7 +10,7 @@ export default () => {
             setCountdown((prevState) => {
                 if (prevState - 1 <= 0) {
                     _id && window.clearInterval(_id)
-                    navigate(`/Login`);
+                    navigate(`/Login`, { replace: true })
                 }
 
                 return prevState - 1
@@ -21,7 +21,7 @@ export default () => {
 
     function onClickLogin() {
         id && window.clearInterval(id)
-        navigate(`/Login`);
+        navigate(`/Login`, { replace: true })
     }
 
     return (
