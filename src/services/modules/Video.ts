381421f94d427992298547dcb87d5e77300d042f
bin.zeng <596659597@qq.com>
2025-03-30 22:12:11 +0800

    update

--- src/services/modules/Video.ts
+++ src/services/modules/Video.ts
@@ -1,32 +1,32 @@
 import {Delete, Get, Post, Put} from '../decorator/http'
 
 export class Video {
-    @Get({url: 'http://localhost:8080/social-api/api/video/list'})
+    @Get({url: 'http://localhost:8081/social-api/api/video/list'})
     static async getVideoList(params: any = {}) {
         return params
     }
 
-    @Get({url: 'http://localhost:8080/social-api/api/video/all'})
+    @Get({url: 'http://localhost:8081/social-api/api/video/all'})
     static async getVideoAll(params: any = {}) {
         return params
     }
 
-    @Get({url: 'http://localhost:8080/social-api/api/video/:id'})
+    @Get({url: 'http://localhost:8081/social-api/api/video/:id'})
     static async findVideo(id: any) {
     }
 
-    @Post({url: 'http://localhost:8080/social-api/api/video'})
+    @Post({url: 'http://localhost:8081/social-api/api/video'})
     static async insertVideo(data: any = {}) {
         return data
     }
 
-    @Put({url: 'http://localhost:8080/social-api/api/video/:id'})
+    @Put({url: 'http://localhost:8081/social-api/api/video/:id'})
     static async updateVideo(id: any, data: any = {}) {
         console.log(id, data)
         return data
     }
 
-    @Delete({url: 'http://localhost:8080/social-api/api/video/:id'})
+    @Delete({url: 'http://localhost:8081/social-api/api/video/:id'})
     static async deleteVideo(id: any) {
         console.log({id})
     }
