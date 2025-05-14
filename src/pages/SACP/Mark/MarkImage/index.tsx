9a0cfa000d3e0503b7823cf94d8484e3af63e023
KEFU\zengweibin <596659597@qq.com>
2025-05-14 10:32:31 +0800

    update: 增加 children的类型未React组件类型

--- src/pages/SACP/Mark/MarkImage/index.tsx
+++ src/pages/SACP/Mark/MarkImage/index.tsx
@@ -1,11 +1,34 @@
-import {useRef, useState} from 'react'
+import {ReactElement} from 'react'
 import './index.scss'
 
+type OnlyTitleSection = ReactElement<typeof TitleSection, typeof TitleSection>;
+
+type ModuleProps = {
+  children: OnlyTitleSection;
+};
+
+export function Module({children}: ModuleProps) {
+  return <section>module:{children}</section>;
+}
+
+export function TitleSection() {
+  return (
+    <section>TitleSection</section>
+  );
+}
 
 export default () => {
-    return (
-        <div className='mark-image-page'>
-            Mark Image
-        </div>
-    )
+  return (
+    <div className='mark-image-page'>
+      Mark Image
+
+      <Module>
+        <div>Should error</div>
+      </Module>
+
+      <Module>
+        Should
+      </Module>
+    </div>
+  )
 }
