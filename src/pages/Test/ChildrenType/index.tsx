117653db527a017afbc0882b7f16b7f4f1ae19e0
KEFU\zengweibin <596659597@qq.com>
2025-05-20 16:10:39 +0800

    update

--- src/pages/Test/ChildrenType/index.tsx
+++ src/pages/Test/ChildrenType/index.tsx
@@ -1,12 +1,21 @@
-import {ReactElement} from 'react'
+import React, {ReactElement, ComponentProps, ReactNode} from 'react'
 
-type OnlyTitleSection = ReactElement<typeof TitleSection, typeof TitleSection>;
-
+// 方法一：使用 JSX.Element 并检查 type
 type ModuleProps = {
-  children: OnlyTitleSection;
+  children: ReactElement<any, typeof TitleSection>;
 };
 
+// 方法二：使用自定义类型检查函数
+function isTitleSection(child: ReactNode): child is ReactElement<any> {
+  return React.isValidElement(child) && child.type === TitleSection;
+}
+
 export function Module({children}: ModuleProps) {
+  // 可以在运行时也进行检查
+  if (!isTitleSection(children)) {
+    console.error('Module children must be TitleSection');
+  }
+
   return <section>module:{children}</section>;
 }
 
@@ -27,9 +36,9 @@ export default () => {
         <div>Should error</div>
       </Module>
 
-      {/*<Module>
+      {/* <Module>
         error
-      </Module>*/}
+      </Module> */}
     </div>
   )
 }
